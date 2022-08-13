2f43596baac6654ff90435be847219377ef62a6a
Marcelo <ricardosilva.marcelo@gmail.com>
2022-08-12 22:17:48 -0300

    cadastro php

---
+++ cadastro.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+        <title>Cadastro de Cliente</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<body>
+        <div class='container'>
+            <fieldset>
+                <legend><h1>Formulário - Cadastro de Cliente</h1><legend>
+
+                <form action="action_cliente.php" method="post" id='form-contato' enctype='multipart/form-data'>
+                    <div class="row">
+                        <label for="nome">Selecionar Foto</label>
+                    <div class="col-md-2">
+                        <a href='#' class="thumbnail">
+                            <img src="img/padrao.jpg" heigth="190" width="150" id="foto-cliente">
+</a> 
+</div>
+
+        <input type="file" name="foto" id="foto" value="foto">
+</div>
+
+                <div class="form-group">
+                    <label for="nome">\\Nome
+
+
